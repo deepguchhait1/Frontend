92f21b9ada806a56646359c830ba465eef10eb95
“username” <emailaddr>
2025-07-25 14:47:15 +0530

    Add the backend link

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://backend-thvw.onrender.com/api",
   withCredentials: true,
 });
